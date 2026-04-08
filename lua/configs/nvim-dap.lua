3b41be9f571bb4c1893e75a90931c5953c44c1d7
barzin144 <barzinassa14477@gmail.com>
2026-04-09 00:48:48 +0800

    improved dap-ui

--- lua/configs/nvim-dap.lua
+++ lua/configs/nvim-dap.lua
@@ -16,33 +16,32 @@ dap.configurations.cs = {
     type = "coreclr",
     name = "launch - netcoredbg",
     request = "launch",
-    -- program = function()
-    --   -- return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/src/", "file")
-    --   return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/bin/Debug/net8.0/", "file")
-    -- end,
-    --
-    -- -- justMyCode = false,
-    -- -- stopAtEntry = false,
-    -- -- -- program = function()
-    -- -- --   -- todo: request input from ui
-    -- -- --   return "/path/to/your.dll"
-    -- -- -- end,
-    -- -- env = {
-    -- --   ASPNETCORE_ENVIRONMENT = function()
-    -- --     -- todo: request input from ui
-    -- --     return "Development"
-    -- --   end,
-    -- --   ASPNETCORE_URLS = function()
-    -- --     -- todo: request input from ui
-    -- --     return "http://localhost:5050"
-    -- --   end,
-    -- -- },
-    -- -- cwd = function()
-    -- --   -- todo: request input from ui
-    -- --   return vim.fn.getcwd()
-    -- -- end,
+    program = function()
+      -- return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/src/", "file")
+      return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/bin/Debug/net8.0/", "file")
+    end,
   },
 }
+
+vim.fn.sign_define("DapBreakpoint", {
+  text = "⚪",
+  texthl = "DapBreakpointSymbol",
+  linehl = "DapBreakpoint",
+  numhl = "DapBreakpoint",
+})
+
+vim.fn.sign_define("DapStopped", {
+  text = "🔴",
+  texthl = "yellow",
+  linehl = "DapBreakpoint",
+  numhl = "DapBreakpoint",
+})
+vim.fn.sign_define("DapBreakpointRejected", {
+  text = "⭕",
+  texthl = "DapStoppedSymbol",
+  linehl = "DapBreakpoint",
+  numhl = "DapBreakpoint",
+})
 
 local map = vim.keymap.set
 
@@ -54,7 +53,6 @@ map("n", "<F9>", "<Cmd>lua require'dap'.toggle_breakpoint()<CR>", opts)
 map("n", "<F10>", "<Cmd>lua require'dap'.step_over()<CR>", opts)
 map("n", "<F11>", "<Cmd>lua require'dap'.step_into()<CR>", opts)
 map("n", "<F8>", "<Cmd>lua require'dap'.step_out()<CR>", opts)
--- map("n", "<F12>", "<Cmd>lua require'dap'.step_out()<CR>", opts)
 map("n", "<leader>dr", "<Cmd>lua require'dap'.repl.open()<CR>", opts)
 map("n", "<leader>dl", "<Cmd>lua require'dap'.run_last()<CR>", opts)
 map(
