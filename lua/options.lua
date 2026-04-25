5a9b045fcf3ed28b295a9321a7d491abbaf55aae
barzin144 <barzinassa14477@gmail.com>
2026-04-25 15:13:00 +0800

    added folding block

--- lua/options.lua
+++ lua/options.lua
@@ -7,3 +7,7 @@ o.cursorlineopt = "both" -- to enable cursorline!
 o.relativenumber = true
 o.spell = true
 o.scrolloff = 30
+o.foldmethod = 'expr'
+o.foldexpr = 'nvim_treesitter#foldexpr()'
+o.foldenable = true
+o.foldlevel = 99
